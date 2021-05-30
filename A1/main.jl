12cdd314775f0bc92330015edef279ca85c7ba2e
Pavel Sobolev <paveloomm@gmail.com>
2021-05-30 15:51:49 +0300

    Add a comparison plot for the first assignment.

--- A1/main.jl
+++ A1/main.jl
@@ -1,21 +1,37 @@
 include("src/A1.jl")
 using .A1
 
-y = solve(
-    Problem(
-        (x) -> 2 / √(x + 1),
-        (x) -> -2 * √(x^2 + 1),
-        (x) -> 10 / (1 + x * (1 - x)),
-        0,
-        0,
-        0.5,
-        1,
-        -1,
-        0,
-    ),
-    Options(1000),
+using GRUtils
+
+a = 0
+b = 1
+
+p = Problem(
+    (x) -> 2 / √(x + 1),
+    (x) -> -2 * √(x^2 + 1),
+    (x) -> 10 / (1 + x * (1 - x)),
+    a,
+    0,
+    0.5,
+    b,
+    -1,
+    0,
 )
 
 open("result", "w") do io
-    println(io, y)
+    println(io, solve(p))
 end
+
+for n in (10, 100)
+    h = (b - a) / n
+    x = [a + i * h for i in 0:n]
+    y = solve(p, Options(n))
+    if n == 10
+        plot(x, y, "+", x, y, "b--", hold=true)
+    else
+        plot(x, y, hold=true)
+    end
+end
+
+legend("n = 10", "n = 10", "n = 100", location = "upper left")
+savefig("figures/comparison.pdf")
